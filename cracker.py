3d2ba4b2e82cad65a3ff374ba808b1492f5de5c0
supremXD <74043675+supremXD@users.noreply.github.com>
2022-07-21 04:15:00 +0200

    Add files via upload

--- cracker.py
+++ cracker.py
@@ -3,7 +3,19 @@ import os, time
 def start_menu():
     os.system("clear")
     while True:
+        options()
+
+def options():
+    print("")
+    print("              |                    1 -->> Extract the hash of the rar")
+    print("              |                    2 -->> Exit")    
+    option = input("              +-> ")
+
+    if option == "1":
         hash()
+
+    if option == "2":
+        exit()
 
 def hash():
     os.system("clear")
@@ -14,13 +26,11 @@ def hash():
     os.system("rar2john rar.rar > hash.txt")
     os.system("sed -i 's/rar.rar://g' hash.txt")
     os.system("mv rar.rar "+filenamehash+"")
-    time.sleep(3)
+    time.sleep(5)
     os.system("clear")
     print("")
-    os.system("hashcat -m 13000 -a3 -o password.txt 'hash.txt' ?d?d?d?d --show")
+    os.system("hashcat -m 13000 -a3 'hash.txt' ?d?d?d?d > password.txt")
     os.system("rm -f hash.txt")
-    print("The password is the next digits of the last ':' of the line (Example: ':0000'")
-    os.system("cat password.txt")
     while True:
         exit()
 
