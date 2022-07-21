0d9331022375d04595d09c7cfb8dbb6b678c8283
supremXD <74043675+supremXD@users.noreply.github.com>
2022-07-21 03:48:45 +0200

    Add files via upload

--- cracker.py
+++ cracker.py
@@ -3,42 +3,25 @@ import os, time
 def start_menu():
     os.system("clear")
     while True:
-        options()
-
-def options():
-    print("              |                    1 -->> Extract the hash of the rar")
-    print("              |                    2 -->> Crack the hash")
-    print("              |                    3 -->> Exit")    
-    option = input("              +-> ")
-
-    if option == "1":
         hash()
-
-    if option == "2":
-        crack()
-
-    if option == "3":
-        exit()
 
 def hash():
     os.system("clear")
     print("")
     filenamehash = input("Complete file name (with path) -----> ")
     os.system("mv "+filenamehash+" rar.rar")
-    os.system("clear")
     os.system("rm -f hash.txt")
     os.system("rar2john rar.rar > hash.txt")
     os.system("sed -i 's/rar.rar://g' hash.txt")
     os.system("mv rar.rar "+filenamehash+"")
-    time.sleep(5)
+    time.sleep(3)
     os.system("clear")
     print("")
-    os.system("hashcat -m 13000 -a3 'hash.txt' ?d?d?d?d")
-
-def crack():
-    os.system("clear")
-    print("")
-    os.system("hashcat -m 13000 -a3 'hash.txt' ?d?d?d?d")
-
+    os.system("hashcat -m 13000 -a3 -o password.txt 'hash.txt' ?d?d?d?d --show")
+    os.system("rm -f hash.txt")
+    print("The password is the next digits of the last ':' of the line (Example: ':0000'")
+    os.system("cat password.txt")
+    while True:
+        exit()
 
 start_menu()
